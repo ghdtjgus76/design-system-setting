d24ef2f5bd6a70d3713af591e6d10e2179684f02
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-25 00:25:55 +0900

    feat: ui 워크스페이스 panda 관련 초기화 작업

---
+++ packages/ui/panda.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  preflight: true,
+  include: [
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./stories/**/*.{js,jsx,ts,tsx}",
+  ],
+  exclude: [],
+  outdir: "styled-system",
+});
