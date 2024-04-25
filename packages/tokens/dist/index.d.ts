5839bd222c0055b1c8d19a5df03dff5814ae70c5
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-25 15:57:21 +0900

    feat: tokens 배포 관련 세팅 및 배포

--- packages/tokens/dist/index.d.ts
+++ packages/tokens/dist/index.d.ts
@@ -1,2 +1,3 @@
 export * as color from "./color.ts";
 export * as typography from "./typography.ts";
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/tokens/dist/typography.d.ts
@@ -0,0 +1,7 @@
+export declare const h1: {
+    fontSize: string;
+};
+export declare const h2: {
+    fontSize: string;
+};
+//# sourceMappingURL=typography.d.ts.map
