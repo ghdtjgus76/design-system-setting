40aec3d197707bdf3a70ffe8be06b42a07892e6e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-25 20:41:49 +0900

    chore: tokens 패키지 npmignore 파일 추가

--- packages/tokens/dist/color.d.ts
+++ packages/tokens/dist/color.d.ts
@@ -1,5 +1,5 @@
-export declare const black = "#000000";
-export declare const white = "#ffffff";
-export declare const red800 = "#e81300";
-export declare const blue800 = "#0a85b5";
+export declare const black = "var(--static-color-static-black)";
+export declare const white = "var(--static-color-static-white)";
+export declare const red800 = "var(--static-color-static-red-800)";
+export declare const blue800 = "var(--static-color-static-blue-800)";
 //# sourceMappingURL=color.d.ts.map
