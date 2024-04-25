7efbd5023141d67c6fe28576ff833ba00452b6f1
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-25 15:20:23 +0900

    feat: tokens 관련 코드 작성

---
+++ packages/tokens/src/color.ts
@@ -0,0 +1,4 @@
+export const black = "var(--static-color-static-black)";
+export const white = "var(--static-color-static-white)";
+export const red800 = "var(--static-color-static-red-800)";
+export const blue800 = "var(--static-color-static-blue-800)";
---
+++ packages/tokens/src/typography.ts
@@ -0,0 +1,7 @@
+export const h1 = {
+  fontSize: "var(--semantic-typography-h1-font-size)",
+};
+
+export const h2 = {
+  fontSize: "var(--semantic-typography-h2-font-size)",
+};
